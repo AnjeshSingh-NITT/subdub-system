1c710adad64d7fa173e6f4b0922366df3290d579
Anjesh Singh <singhanjesh2005@gmail.com>
2025-12-29 17:22:58 +0000

    feat: add MongoDB connection and update dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import 'dotenv/config';
 import app from './app.js';
+import connectDB from './database/index.js';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+console.log("MONGODB_URI:", process.env.MONGODB_URI ? "Loaded" : "Not loaded");
+
+connectDB()
+.then(() => {
+    const server = app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+    server.on("error", (error) => {
+        console.error("Server error:", error);
+    });
+})
+.catch((error) => {
+    console.error("FAILED to connect to the database:", error);
 });
